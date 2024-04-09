5becba507a06bc5dd9af630cf463ea5ac47a513c
isaacsnewaccount <143953607+isaacsnewaccount@users.noreply.github.com>
2024-04-09 13:15:29 -0400

    Enable wireframe mode

--- src/main.cpp
+++ src/main.cpp
@@ -64,6 +64,8 @@ int main(void) {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
