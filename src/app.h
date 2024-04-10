3c7a7d87ef130e58991b9a128d1d4cd4e86fa6fe
isaacsnewaccount <143953607+isaacsnewaccount@users.noreply.github.com>
2024-04-10 15:34:46 -0400

    Add application class files (empty)

---
+++ src/app.h
@@ -0,0 +1,23 @@
+// #pragma once
+
+// #include <iostream>
+
+
+class Application {
+public:
+    // Constructor
+    Application() {
+
+    }
+};
+
+
+// #ifndef message_h
+// #define message_h
+
+// class Message {
+// public:
+//     void printMessage();
+// };
+
+// #endif //message_h
