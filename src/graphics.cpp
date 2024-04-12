c8249ebb644268c3eccaa7c452706458cdb46f60
isaacsnewaccount <143953607+isaacsnewaccount@users.noreply.github.com>
2024-04-12 12:38:54 -0400

    Comment out unused function contents

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -128,25 +128,25 @@ void setupVertexArray(unsigned int VAO, unsigned int VBO) {
 
 void doTextureThing() {
 
-    // Define pixel data
-    unsigned char pixelData[4] = {255, 0, 0, 255}; // RGBA format (red color)
+    // // Define pixel data
+    // unsigned char pixelData[4] = {255, 0, 0, 255}; // RGBA format (red color)
 
-    // Create texture to hold pixel data
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
+    // // Create texture to hold pixel data
+    // GLuint texture;
+    // glGenTextures(1, &texture);
+    // glBindTexture(GL_TEXTURE_2D, texture);
 
-    // Set texture parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    // // Set texture parameters
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    // glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
-    // Update texture with pixel data
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
+    // // Update texture with pixel data
+    // glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, 1, 1, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
 
-    // Draw a quad with the pixel as a texture
-// You need to define vertices and texture coordinates for the quad, then draw it using OpenGL functions like glDrawArrays or glDrawElements
+    // // Draw a quad with the pixel as a texture
+    // // You need to define vertices and texture coordinates for the quad, then draw it using OpenGL functions like glDrawArrays or glDrawElements
 
 
 }
